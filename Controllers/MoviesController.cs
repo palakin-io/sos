4b76c4de85efeece75eae3527a2a55d6e0c9fb16
palakin-io <57917679+palakin-io@users.noreply.github.com>
2019-11-25 13:51:50 -0300

    Add files via upload

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,53 @@
+﻿using AccentureMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccentureMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private AccentureAccademyMovieDbEntities db;
+        public MoviesController()
+        {
+            this.db = new AccentureAccademyMovieDbEntities();
+        }
+
+        public ActionResult Listar()
+        {
+            List<Movie> movies = this.db.Movie.ToList();
+            return View(movies);
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Movie m = this.db.Movie.Find(id);
+            return View(m);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Movie movie)
+        {
+            this.db.Movie.Attach(movie);
+            this.db.Entry(movie).State = System.Data.Entity.EntityState.Modified;
+            this.db.SaveChanges();
+            return RedirectToAction("Listar");
+        }
+
+        public ActionResult Agregar()
+        {
+            Movie m = new Movie();
+            return View("Editar", m);
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(Movie movie)
+        {
+            this.db.Movie.Add(movie);
+            this.db.SaveChanges();
+            return RedirectToAction("Listar");
+        }
+    }
+}
---
+++ Script.sql
@@ -0,0 +1,21 @@
+﻿CREATE DATABASE AccentureAccademyMovieDb;
+GO
+
+USE AccentureAccademyMovieDb;
+GO
+
+CREATE TABLE Movie
+(
+	id int primary key identity (1,1),
+	Title Varchar(200) not null,
+	ReleaseDate date not null,
+	runningTime int not null,
+	CONSTRAINT UQ_TITLE UNIQUE(Title, ReleaseDate),
+	CONSTRAINT CHK_DURATION CHECK(runningTime between 1 and 500)
+);
+GO
+
+INSERT INTO Movie
+	(Title, ReleaseDate, runningTime)
+VALUES
+	('Terminator', '2019-10-31', 128)
